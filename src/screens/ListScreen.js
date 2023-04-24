03f3da757af4277772280017380fb102bcbf5229
Marcellus <majordanduke22@gmail.com>
2023-04-23 20:10:38 -0400

    Enabled Prettier auto format.

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import { Text, StyleSheet, FlatList } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
-    const friends = [
-        { name: 'Friend #1', age: 20},
-        { name: 'Friend #2', age: 25},
-        { name: 'Friend #3', age: 22},
-        { name: 'Friend #4', age: 21},
-        { name: 'Friend #5', age: 19}, 
-        { name: 'Friend #6', age: 27},
-        { name: 'Friend #7', age: 31},
-        { name: 'Friend #8', age: 24},
-        { name: 'Friend #9', age: 22}
-    ];
+  const friends = [
+    { name: "Friend #1", age: 20 },
+    { name: "Friend #2", age: 25 },
+    { name: "Friend #3", age: 22 },
+    { name: "Friend #4", age: 21 },
+    { name: "Friend #5", age: 19 },
+    { name: "Friend #6", age: 27 },
+    { name: "Friend #7", age: 31 },
+    { name: "Friend #8", age: 24 },
+    { name: "Friend #9", age: 22 },
+  ];
 
-    return (
-        
-        <FlatList
-            showsVerticalScrollIndicator={false} 
-            keyExtractor={(friend) => friend.name}
-            data={friends} 
-            renderItem={({ item }) => {
-                return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>;
-            }}
-        />
-    );
+  return (
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(friend) => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    textStyle: {
-        marginVertical: 50,
-        fontSize: 40
-    }
+  textStyle: {
+    marginVertical: 50,
+    fontSize: 40,
+  },
 });
 
 export default ListScreen;
